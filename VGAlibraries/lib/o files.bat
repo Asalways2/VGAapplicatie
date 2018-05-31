0ecffe51b60d9378b8304a6abd13f5f0b561b9dc
Remy van der Pol <>
2018-05-31 15:10:34 +0200

    Reset linker config to use API lib source

--- VGAlibraries/lib/o files.bat
+++
@@ -1,9 +0,0 @@
-gcc -c       API_draw/draw.c    -o draw.o
-gcc -c       API_io/api_io.c -o api_io.o
-
-#
-# object files for shared libraries need to be compiled as position independent
-# code (-fPIC) because they are mapped to any position in the address space.
-#
-gcc -c -fPIC API_draw/draw.c    -o draw.o
-gcc -c -fPIC API_io/api_io.c -o api_io.o
